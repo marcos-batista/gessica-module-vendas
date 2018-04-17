eb081b71f56aebb6c7bb7152d899d82602ca7a33
Marcos Batista <mab.marcos@gmail.com>
2018-04-17 00:28:51 -0300

    Applying correct files

--- src/main/java/com/agroall/gessica/vendas/VendasModule.java
+++ src/main/java/com/agroall/gessica/vendas/VendasModule.java
@@ -1,12 +1,10 @@
 package com.agroall.gessica.vendas;
 
-import org.springframework.boot.SpringApplication;
-
 //@SpringBootApplication
 public class VendasModule {
 	
-	public static void main(String[] args) {
-		SpringApplication.run(VendasModule.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(VendasModule.class, args);
+//	}
 	
 }
