178edb13b0ee5869eaf1981afd8c2d6d77edc63f
Marcos Batista <mab.marcos@gmail.com>
2018-04-16 23:08:36 -0300

    Delete all the files

--- src/main/java/com/agroall/gessica/vendas/dataobjects/ItemOrdemVenda.java
+++
@@ -1,7 +0,0 @@
-package com.agroall.gessica.vendas.dataobjects;
-
-import com.agroall.gessica.dataobjects.ItemVenda;
-
-public class ItemOrdemVenda extends ItemVenda {
-
-}
--- src/main/java/com/agroall/gessica/vendas/dataobjects/ProdutoComercial.java
+++
@@ -1,5 +0,0 @@
-package com.agroall.gessica.vendas.dataobjects;
-
-public class ProdutoComercial extends com.agroall.gessica.dataobjects.ProdutoComercial {
-
-}
