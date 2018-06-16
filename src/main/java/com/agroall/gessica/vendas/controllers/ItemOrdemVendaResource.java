53d1c7565a2a834e4218959b284c6a6915de2a51
Marcos Batista <mab.marcos@gmail.com>
2018-06-16 16:39:06 -0300

    Implementacao de servico de Item Ordem Venda

---
+++ src/main/java/com/agroall/gessica/vendas/controllers/ItemOrdemVendaResource.java
@@ -0,0 +1,32 @@
+package com.agroall.gessica.vendas.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.agroall.gessica.controllers.DataObjectResourceControllerImpl;
+import com.agroall.gessica.services.Service;
+import com.agroall.gessica.vendas.dataobjects.ItemOrdemVenda;
+import com.agroall.gessica.vendas.services.ItemOrdemVendaService;
+
+@RestController
+@RequestMapping("/ordemvenda/item")
+@CrossOrigin
+public class ItemOrdemVendaResource extends DataObjectResourceControllerImpl<ItemOrdemVenda> {
+	
+	@Autowired private ItemOrdemVendaService service;
+	
+	@Override
+	protected Service<ItemOrdemVenda> getService() {
+		return this.service;
+	}
+	
+	@Override
+	@RequestMapping(method = RequestMethod.GET)
+	public ItemOrdemVenda doGet() {
+		return super.doGet();
+	}
+	
+}
