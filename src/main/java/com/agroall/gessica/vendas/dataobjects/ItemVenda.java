c88fc45712be981c50b03ec5fa7722bb90d3bb2f
Marcos Batista <mab.marcos@gmail.com>
2018-06-24 18:29:02 -0300

    Corrigindo bug de ItemVenda

--- src/main/java/com/agroall/gessica/vendas/dataobjects/ItemVenda.java
+++ src/main/java/com/agroall/gessica/vendas/dataobjects/ItemVenda.java
@@ -4,6 +4,7 @@ public class ItemVenda extends com.agroall.gessica.dataobjects.ItemVenda {
 	
 	private Double margemLucroItem;
 	private Double descontoItem;
+	private Double precoCalculado2;
 	
 	private ProdutoComercial produtoComercial;
 	
@@ -19,7 +20,7 @@ public class ItemVenda extends com.agroall.gessica.dataobjects.ItemVenda {
 	
 	public void setMargemLucroItem(Double margemLucroItem) {
 		this.margemLucroItem = margemLucroItem;
-		this.precoCalculado = resolvePrecoCalculado();
+		this.precoCalculado2 = resolvePrecoCalculado();
 	}
 	
 	public Double getDescontoItem() {
@@ -28,7 +29,17 @@ public class ItemVenda extends com.agroall.gessica.dataobjects.ItemVenda {
 	
 	public void setDescontoItem(Double descontoItem) {
 		this.descontoItem = descontoItem;
-		this.precoCalculado = resolvePrecoCalculado();
+		this.precoCalculado2 = resolvePrecoCalculado();
+	}
+	
+	@Override
+	public Double getPrecoCalculado() {
+		return this.precoCalculado2;
+	}
+	
+	@Override
+	public void setPrecoCalculado(Double precoCalculado) {
+		this.precoCalculado2 = resolvePrecoCalculado();
 	}
 	
 	public ProdutoComercial getProdutoComercial() {
@@ -42,13 +53,12 @@ public class ItemVenda extends com.agroall.gessica.dataobjects.ItemVenda {
 		setPrecoItem(precoItem);
 	}
 	
-	@Override
-	protected Double resolvePrecoCalculado() {
+	public Double resolvePrecoCalculado() {
 		Double precoCalculado = super.resolvePrecoCalculado();
 		if(this.descontoItem == null) { return precoCalculado; }
 		if(this.margemLucroItem == null) { return precoCalculado; }
 		precoCalculado = ((precoCalculado - this.descontoItem) + this.margemLucroItem);
-		this.precoCalculado = precoCalculado;
+		this.precoCalculado2 = precoCalculado;
 		return precoCalculado;
 	}
 	
