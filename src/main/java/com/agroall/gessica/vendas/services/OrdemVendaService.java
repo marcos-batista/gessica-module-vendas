f1e361418eaaccd0767181fe8f3031e36eaaa543
Marcos Batista <mab.marcos@gmail.com>
2018-06-24 19:04:01 -0300

    Finalizando requisito 'Informar venda realizada'

--- src/main/java/com/agroall/gessica/vendas/services/OrdemVendaService.java
+++ src/main/java/com/agroall/gessica/vendas/services/OrdemVendaService.java
@@ -1,11 +1,15 @@
 package com.agroall.gessica.vendas.services;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.agroall.gessica.repositories.Repository;
 import com.agroall.gessica.services.ServiceImpl;
+import com.agroall.gessica.vendas.dataobjects.ItemOrdemVenda;
 import com.agroall.gessica.vendas.dataobjects.OrdemVenda;
+import com.agroall.gessica.vendas.dataobjects.ProdutoComercial;
 import com.agroall.gessica.vendas.repositories.OrdemVendaRepository;
 
 @Service
@@ -28,14 +32,15 @@ public class OrdemVendaService extends ServiceImpl<OrdemVenda> {
 		return this.repository;
 	}
 	
-//	protected void validateOrdemVendaBeforeSave(OrdemVenda ordemVenda) {
-//		if(ordemVenda == null) throw new RuntimeException("A ordem de venda não foi informada");
-//		if(ordemVenda.getItens() == null || ordemVenda.getItens().isEmpty()) throw new RuntimeException("Os itens da ordem de venda não foram informados");
-//		Collection<ItemOrdemVenda> itensOrdemVenda = ordemVenda.getItens();
-//		for (ItemOrdemVenda itemOrdemVenda : itensOrdemVenda) {
-//			if(itemOrdemVenda.getProdutoComercial() == null) throw new RuntimeException("Há pelo menos um item da ordem de venda cujo produto não foi informado");
-//			if(itemOrdemVenda.getProdutoComercial().getId() == null) throw new RuntimeException("Há pelo menos um item da ordem de venda cujo produto é inválido");
-//		}
-//	}
+	@Override
+	protected void validateBeforeInsertOrUpdate(OrdemVenda ordemVenda) {
+		super.validateBeforeInsertOrUpdate(ordemVenda);
+		Collection<ItemOrdemVenda> itensOrdemVenda = ordemVenda.getItens();
+		if(itensOrdemVenda == null) return;
+		for (ItemOrdemVenda itemOrdemVenda : itensOrdemVenda) {
+			ProdutoComercial produtoComercial = itemOrdemVenda.getProdutoComercial();
+			if(produtoComercial == null) throw new RuntimeException("Existem itens da venda cujo produto não foi informado.");
+		}
+	}
 	
 }
