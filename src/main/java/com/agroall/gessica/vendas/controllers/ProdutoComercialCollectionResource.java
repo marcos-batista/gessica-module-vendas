6d1d70283fe2b56454cd784df579c0ff616769fd
Marcos Batista <mab.marcos@gmail.com>
2018-06-24 21:50:19 -0300

    Implementacao do caso de uso de baixa de estoque

--- src/main/java/com/agroall/gessica/vendas/controllers/ProdutoComercialCollectionResource.java
+++ src/main/java/com/agroall/gessica/vendas/controllers/ProdutoComercialCollectionResource.java
@@ -3,6 +3,7 @@ package com.agroall.gessica.vendas.controllers;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.agroall.gessica.vendas.dataobjects.ProdutoComercial;
 import com.agroall.gessica.vendas.services.ProdutoComercialService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/produtos")
 public class ProdutoComercialCollectionResource extends DataCollectionResourceControllerImpl<ProdutoComercial> {
 	
