1e2b3d1a1e86ac9b4f0b1062d4f629c145625744
Marcos Batista <mab.marcos@gmail.com>
2018-06-30 22:35:11 -0300

    Inclusao de idade para Cliente

--- src/main/java/com/agroall/gessica/vendas/dataobjects/Cliente.java
+++ src/main/java/com/agroall/gessica/vendas/dataobjects/Cliente.java
@@ -9,6 +9,7 @@ import com.agroall.gessica.dataobjects.aspects.Persistent;
 public class Cliente extends com.agroall.gessica.dataobjects.Cliente implements Persistent<String> {
 	
 	@Id private String id;
+	private int idade;
 	
 	@Override
 	public String getId() {
@@ -18,6 +19,14 @@ public class Cliente extends com.agroall.gessica.dataobjects.Cliente implements 
 	@Override
 	public void setId(String id) {
 		this.id = (String) id;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 	
 }
